cf3cc4d4df02307d2ec758c9042cc592e7c90195
朱睿冬 <pku1800011036@pku.edu.cn>
2022-04-06 15:50:39 +0800

    fetch time

--- ycsb/src/rocksdb_client.cc
+++ ycsb/src/rocksdb_client.cc
@@ -218,11 +218,16 @@ void RocksDBClient::RocksDBWorker(uint64_t num, int coreid, bool is_warmup, bool
 		double time =  TIME_DURATION(start, TIME_NOW);
 		request_time.Insert(time);
 		static int t = 0;
+		static int tt = 0;
 		if(opt == READ || opt == SCAN){
 			read_time.Insert(time);
+			if(tt < 10) {
+				printf("time: %lf, now: %lf\n", time, TIME_NOW);
+				tt++;
+			}
 			if(time > 1000) {
 				if(t<10) {
-					printf("time: %lf, now: %lf\n", time, TIME_NOW);
+					printf("long time: %lf, now: %lf\n", time, TIME_NOW);
 					t++;
 				}
 			}
